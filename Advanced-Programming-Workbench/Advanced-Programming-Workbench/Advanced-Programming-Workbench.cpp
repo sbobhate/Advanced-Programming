53e9ee25181248280844f0ca7e9f5be50588560e
Shantanu Bobhate <6529864+sbobhate@users.noreply.github.com>
2019-03-31 20:32:31 -0400

    Workbench setup

---
+++ Advanced-Programming-Workbench/Advanced-Programming-Workbench/Advanced-Programming-Workbench.cpp
@@ -0,0 +1,14 @@
+// Advanced-Programming-Workbench.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdio.h>
+
+int main()
+{
+	printf("Hello World!\n");
+	scanf("");
+
+    return 0;
+}
+
